c2ed1d5f30959493aba2469905571c6d2e601410
Piradon <piradonleungamornnara@gmail.com>
2026-07-21 16:24:53 +0700

    feat: Implement assignment creation endpoint and update database connection string

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,37 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.post("/assignments", async (req, res) => {
+  try {
+    const { title, content, category } = req.body;
+
+    if (!title || !content || !category) {
+      return res.status(400).json({
+        message:
+          "Server could not create assignment because there are missing data from client",
+      });
+    }
+
+    await connectionPool.query(
+      "INSERT INTO assignments (title, content, category) VALUES ($1, $2, $3) RETURNING *",
+      [title, content, category],
+    );
+
+    return res.status(201).json({ message: "Created assignment sucessfully" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server could not create assignment because database connection",
+    });
+  }
 });
 
 app.listen(port, () => {
--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:piradon493328@localhost:5432/data_API",
 });
 
 export default connectionPool;
